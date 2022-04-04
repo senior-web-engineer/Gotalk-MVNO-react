292a4576c661edca19bd7e42325f9d193a49957f
ysaribulut <yunusaribulut@gmail.com>
2022-04-04 23:47:33 +0300

    test email changed

--- config/app.config.js
+++ config/app.config.js
@@ -3,5 +3,5 @@ module.exports = {
     debug: process.env.APP_DEBUG || true,
     appUrl: process.env.APP_URL ||  "test-secret",
     port: process.env.APP_PORT || 3000,
-    defaultEmail: process.env.DEFAULT_EMAIL || "bobrow.vadim2017@yandex.ru",
+    defaultEmail: process.env.DEFAULT_EMAIL || "yunusarireklam@gmail.com",
 };
